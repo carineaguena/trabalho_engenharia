95b2397a8cb2ed05014b148d71b9a91636873715
carineaguena <cca6@cin.ufpe.br>
2015-06-18 12:06:05 +0000

    exercicios modificados ate o dia 17/06

--- lib/attr_accessor_with_history.rb
+++ lib/attr_accessor_with_history.rb
@@ -2,9 +2,23 @@ class Class
   def attr_accessor_with_history(attr_name)
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
-    attr_reader attr_name+"_history" # create bar_history getter
+    attr_reader attr_name + "_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+      def #{attr_name}=(attr_name)  
+        @#{attr_name} = attr_name  
+        unless @#{attr_name + "_history"}  
+          @#{attr_name + "_history"} = []  
+          @#{attr_name + "_history"} << nil  
+        end  
+          @#{attr_name + "_history"} << attr_name  
+  
+      end  
     }
   end
+  
+end
+
+
+class Foo 
+  attr_accessor_with_history :bar
 end
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -3,12 +3,36 @@ class RockPaperScissors
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
 
-  def self.winner(player1, player2)
-    # YOUR CODE HERE
-  end
+def self.winner(player1, player2)
+    a = player1.last.downcase
+    b = player2.last.downcase
+    case
+    when a == b then
+      return player1
+    when a == "r" && b == "s" then
+      return player1
+    when a == "s" && b == "p" then
+      return player1
+    when a == "p" && b== "r" then
+      return player1
+    when b == "r" && a == "s" then
+      return player2
+    when b == "s" && a == "p" then
+      return player2
+    when b == "p" && a== "r" then
+      return player2
+    else 
+      raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+    end
+end
+
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if tournament[0].is_a?(String) && tournament[1].is_a?(String)
+        return tournament
+    else
+       return self.winner(self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
+    end
   end
 
 end
